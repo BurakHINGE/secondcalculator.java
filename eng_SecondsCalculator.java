8840cc9f53bf8de1453d2ef0782d56a3eddca958
Mehmet Burak Menteşe <burakmentese16@gmail.com>
2025-12-23 22:03:31 +0300

    Update and rename eng_second_calculate.java to eng_SecondsCalculator.java
    Bug has been fixed.

--- eng_SecondsCalculator.java
+++ eng_SecondsCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SecondsCalculator {
+public class eng_SecondsCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
